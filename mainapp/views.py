4d4d8c900a86e86b8e4dc66cf76cfc0836ae281c
whatisloveT <whatislove56@icloud.com>
2022-05-22 18:46:27 +0500

    lesson_2 Static site with Django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,25 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def hello_world(request):
-    return HttpResponse("Hello world")
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
 
 
-def check_kwargs(request, **kwargs):
-    return HttpResponse(f"kwargs:<br>{kwargs}")
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
